953a47fb81c6be8335a5f8c9a0d1410d5917e582
Victor Vidal <vicemanoel@hotmail.com>
2025-07-11 12:20:13 -0300

    Update gin_handlers.go

--- infrastructure/gin_handlers.go
+++ infrastructure/gin_handlers.go
@@ -2,7 +2,7 @@
 package infrastructure
 
 import (
-    "your_project/usecase" // Ajuste o caminho do import
+    "your_project/usecase" 
     "github.com/gin-gonic/gin"
     "net/http"
     "strconv"
@@ -10,8 +10,8 @@ import (
 
 type VideoHandlers struct {
     UploadVideoUC    *usecase.UploadVideoUseCase
-    ListVideoStatusUC *usecase.ListVideoStatusUseCase // Crie este use case
-    DownloadVideoUC  *usecase.DownloadVideoUseCase   // Crie este use case
+    ListVideoStatusUC *usecase.ListVideoStatusUseCase 
+    DownloadVideoUC  *usecase.DownloadVideoUseCase  
 }
 
 func NewVideoHandlers(uploadUC *usecase.UploadVideoUseCase, listUC *usecase.ListVideoStatusUseCase, downloadUC *usecase.DownloadVideoUseCase) *VideoHandlers {
@@ -51,4 +51,3 @@ func (h *VideoHandlers) UploadVideoHandler(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{"message": output.Message, "filename": output.Filename, "video_status_id": output.VideoStatusID})
 }
 
-// Implemente ListVideoStatusHandler e DownloadProcessedVideoHandler similarmente
